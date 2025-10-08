7281942918bbffc49bb69642e010c40625b023f6
Arun Madhavan <arun_madhavan@comcast.com>
2025-10-08 18:43:42 -0400

    Add missing declaration

--- include/thunder/ProtocolHandler.h
+++ include/thunder/ProtocolHandler.h
@@ -48,6 +48,7 @@ bool parseJson(const string &jsonMsg, Json::Value &root);
 bool convertResultStringToArray(const string &root, const string key, vector<string> &arr);
 bool convertResultStringToBool(const string &root, bool &);
 bool convertResultStringToBool(const string &jsonMsg, const string &key, bool &response);
+bool isJsonRpcResultNull(const string &jsonMsg);
 bool convertEventSubResponseToInt(const string &root, int &);
 bool getValueOfKeyFromJson(const string &jsonMsg, const string &key, string &value);
 string getClientListToJson(int &id);
